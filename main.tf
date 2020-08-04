d048cdb358edd90faf688764c2e765e6072dab1b
Santosh <ssalimath7@gmail.com>
2020-08-04 23:23:32 +0530

    fix SG and instance profile and script

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
+
 provider "aws" {
-  profile = "default"
-  region  = "us-ap-south-1"
+  region  = "ap-south-1"
 }
 
-
 resource "aws_instance" "instance" {
-  key_name      = "my-first-key-pair"
-  ami           = "ami-04590e7389a6e577c"
-  instance_type = "t2.medium"
-  user_data = "${file("bootstrap_script.sh")}"
-  
+  key_name             = "my-first-key-pair"
+  ami                  = "ami-0b5bff6d9495eff69"
+  instance_type        = "t2.medium"
+  user_data            = "${file("bootstrap_script.sh")}"
+  vpc_security_group_ids = ["sg-0a6d1ad9f4e41a468"]
+  iam_instance_profile = "admin"
 }
 
