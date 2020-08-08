ad6668d332fdb2b10afed92c473ac0acdf6929f4
Santosh <ssalimath7@gmail.com>
2020-08-08 08:45:44 +0530

    seperate scripts for ubuntu and amazon based AMI

--- ubuntu-ami/bootstrap_script.sh
+++ ubuntu-ami/bootstrap_script.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-sudo yum upgrade -y
-sudo yum install docker -y
-sudo yum install git -y
-sudo usermod -aG docker ec2-user
+sudo apt-get update -y
+sudo apt-get install docker.io -y
+sudo apt-get install git -y
+sudo apt-get install maven
+sudo usermod -aG docker ubuntu
 sudo service docker start
 sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins:2.235.3-lts-alpine
 sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:community
-#sudo wget http://mirror.serverion.com/jenkins/war-stable/2.235.1/jenkins.war
---
+++ ubuntu-ami/main.tf
@@ -0,0 +1,15 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "instance" {
+  key_name = "my-first-key-pair"
+  #for Ubuntu AMI
+  ami                    = "ami-02d55cb47e83a99a0"
+  instance_type          = "t2.medium"
+  user_data              = "${file("bootstrap_script.sh")}"
+  vpc_security_group_ids = ["sg-0a6d1ad9f4e41a468"]
+  iam_instance_profile   = "admin"
+}
+
