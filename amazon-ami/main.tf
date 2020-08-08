410d738df9c1ffabf027a6dcad62b97c4ab5eef9
Santosh <ssalimath7@gmail.com>
2020-08-08 10:52:13 +0530

    Updated to created t2.xlarge instead of t2.medium

--- amazon-ami/main.tf
+++ amazon-ami/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "instance" {
   key_name = "my-first-key-pair"
   #for Amazon AMI
   ami                  = "ami-0b5bff6d9495eff69"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.xlarge"
   user_data              = "${file("bootstrap_script.sh")}"
   vpc_security_group_ids = ["sg-0a6d1ad9f4e41a468"]
   iam_instance_profile   = "admin"
--- ubuntu-ami/main.tf
+++ ubuntu-ami/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "instance" {
   key_name = "my-first-key-pair"
   #for Ubuntu AMI
   ami                    = "ami-02d55cb47e83a99a0"
-  instance_type          = "t2.medium"
+  instance_type          = "t2.xlarge"
   user_data              = "${file("bootstrap_script.sh")}"
   vpc_security_group_ids = ["sg-0a6d1ad9f4e41a468"]
   iam_instance_profile   = "admin"
