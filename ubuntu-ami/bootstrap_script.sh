1b714dd3115541672528b6d79065a170d12da223
guyinthechair <ssalimath7@gmail.com>
2021-03-11 00:11:36 +0530

    change key pair

--- ubuntu-ami/bootstrap_script.sh
+++ ubuntu-ami/bootstrap_script.sh
@@ -6,4 +6,5 @@ sudo apt-get install maven
 sudo usermod -aG docker ubuntu
 sudo service docker start
 sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins:2.235.3-lts-alpine
-sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:community
+
+#sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:community
