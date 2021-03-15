9379384aa8e0dbfc86aac2b7088062b2d5125413
Santosh Salimath <ssalimath7@gmail.com>
2021-03-15 23:14:15 +0530

    change instance type

--- ubuntu-ami/bootstrap_script.sh
+++ ubuntu-ami/bootstrap_script.sh
@@ -7,4 +7,3 @@ sudo usermod -aG docker ubuntu
 sudo service docker start
 sudo docker run -d --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins:2.235.3-lts-alpine
 
-#sudo docker run -d --name sonarqube -p 9000:9000 sonarqube:community
--- ubuntu-ami/main.tf
+++ ubuntu-ami/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "instance" {
   key_name = "tf-demo"
   #for Ubuntu AMI
   ami                    = "ami-02d55cb47e83a99a0"
-  instance_type          = "t2.xlarge"
+  instance_type          = "t2.medium"
   user_data              = file("bootstrap_script.sh")
   vpc_security_group_ids = ["sg-0a6d1ad9f4e41a468"]
   iam_instance_profile   = "admin"
