ea7a3f125f568be23fe4746fb46942296ce08786
Santosh Salimath <ssalimath7@gmail.com>
2021-04-29 23:16:51 +0530

    change to new accnt

--- amazon-ami/main.tf
+++ amazon-ami/main.tf
@@ -1,15 +1,13 @@
-
 provider "aws" {
   region = "ap-south-1"
 }
 
 resource "aws_instance" "instance" {
-  key_name = "my-first-key-pair"
-  #for Amazon AMI
+  key_name = "tf-demo"
   ami                  = "ami-0b5bff6d9495eff69"
-  instance_type          = "t2.xlarge"
+  instance_type          = "t2.medium"
   user_data              = "${file("bootstrap_script.sh")}"
-  vpc_security_group_ids = ["sg-0a6d1ad9f4e41a468"]
-  iam_instance_profile   = "admin"
+  vpc_security_group_ids = ["sg-0023eb9ce27cd9f8b"]
+  iam_instance_profile   = "admin-test"
 }
 
